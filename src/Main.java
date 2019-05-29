979a1e706ef881e9595b7ddf4b51efe585d7eae8
Alexander Melnichuk <alexander.melnichuk@aurea.com>
2019-05-29 23:20:18 +1000

    Changed if to for

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
     static Integer a = 1;
     
     public static void main(String[] args) {
-        if (a == 0) {
+        for (int i = 0; i < a; i++) {
             Integer b = a;
-        } else {
-            Integer c = a;
+            b = 3;
         }
+        
         for (int i = 0; i < a; i++) {
             Integer b = a;
             Integer c = a;
